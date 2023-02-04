91a80387fe74049db37b3fdd7e9f89fc16d25dd6
Teva Barzilay <tevabar@gmail.com>
2023-02-04 02:53:39 -0500

    About me navigation

--- components/AboutInfo.tsx
+++ components/AboutInfo.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AboutContext } from '../pages/about';
+import { menuSpeed } from '@/styles/GlobalVariables';
+import styled from '@emotion/styled';
+
+interface AboutContextType {
+    index: number;
+    setIndex: React.Dispatch<React.SetStateAction<number>>;
+    info: Array<{ fact: string; title: string; body: string }>;
+}
 
 interface AboutInfoProps {
     fact?: 'default' | 'ireland' | 'jungle' | 'rocky';
 }
 
+const StyledButton = styled.button((props: { hideButton: boolean }) => ({}));
+
 const AboutInfo: React.FC<AboutInfoProps> = () => {
+    const context = useContext(AboutContext);
+    if (context === null) return null;
+    const { index, setIndex, info } = context as AboutContextType;
+
+    const [hideButton, setHideButton] = useState(false);
+
+    useEffect(() => {
+        setHideButton(true);
+        setTimeout(() => {
+            setHideButton(false);
+        }, menuSpeed);
+    }, [index]);
+
+    const FactButtons = () => {
+        return (
+            <>
+                <StyledButton
+                    onClick={() =>
+                        setIndex(index === info.length - 1 ? 0 : index + 1)
+                    }
+                    hideButton={hideButton}
+                    disabled={hideButton ? true : false}
+                >
+                    {index === info.length - 1
+                        ? 'Back to the top'
+                        : index === 0
+                        ? 'Click here for some fun facts about me'
+                        : 'To the next fun fact'}
+                </StyledButton>
+            </>
+        );
+    };
+
     return (
         <>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-            <br></br>
+            <h2>{info[index].title}</h2>
+            <p>{info[index].body}</p>
+            <FactButtons />
         </>
     );
 };
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import AboutImage from '../components/AboutImage';
 import AboutInfo from '../components/AboutInfo';
 import { Column, Wrapper } from '../styles/Theme';
@@ -9,11 +9,43 @@ const About = () => {
     const [localBackground, setLocalBackground] = useState('default');
     const [prevBackground, setPrevBackroud] = useState(null);
     const [swipe, setSwipe] = useState(false);
+    const [index, setIndex] = useState(0);
+
+    const info = [
+        {
+            fact: 'default',
+            title: `Hi! I'm Teva`,
+            body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`,
+        },
+        {
+            fact: 'ireland',
+            title: `I used to be professional Irish Dancer`,
+            body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`,
+        },
+        {
+            fact: 'jungle',
+            title: `I collect plants`,
+            body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`,
+        },
+        {
+            fact: 'rocky',
+            title: `I used to play Frank-N-Furter in a Rocky Horror Picture Show cast`,
+            body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`,
+        },
+    ];
 
     const handleImageChange = (value) => {
         setPrevBackroud(localBackground);
         setLocalBackground(value);
     };
+
+    useEffect(() => {
+        handleImageChange(info[index].fact);
+    }, [index]);
 
     return (
         <AboutContext.Provider
@@ -24,26 +56,14 @@ const About = () => {
                 setSwipe,
                 prevBackground,
                 setPrevBackroud,
+                index,
+                setIndex,
+                info,
             }}
         >
             <Wrapper variant='full'>
                 <Wrapper variant='flex'>
                     <Column variant='wide'>
-                        prev: {prevBackground} current: {localBackground}
-                        <br />
-                        <button onClick={() => handleImageChange('default')}>
-                            default
-                        </button>
-                        <button onClick={() => handleImageChange('ireland')}>
-                            ireland
-                        </button>
-                        <button onClick={() => handleImageChange('jungle')}>
-                            jungle
-                        </button>
-                        <button onClick={() => handleImageChange('rocky')}>
-                            rocky
-                        </button>
-                        <br />
                         <AboutInfo />
                     </Column>
                     <Column variant='narrowAbout'>
